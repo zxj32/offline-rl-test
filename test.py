f5a83d801b6390461f3ac6f551287e271be11e9c
zxj32 <zhaoxuj32@gmail.com>
2026-03-10 17:49:25 -0400

    final_code

--- test.py
+++ test.py
@@ -10,10 +10,3 @@ print(torch.tensor([1, 2, 3, 4, 5]).numpy())
 
 print(torch.tensor([1, 2, 3, 4, 5]).tolist())
 
-print(torch.tensor([1, 2, 3, 4, 5]).to(torch.float32))
-
-print(torch.tensor([1, 2, 3, 4, 5]).to(torch.float64))
-
-print(torch.tensor([1, 2, 3, 4, 5]).to(torch.int32))
-
-print(torch.tensor([1, 2, 3, 4, 5]).to(torch.int64))
