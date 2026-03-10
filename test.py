ab7e6f5801c988ee9a3389cbea0b108c8426981c
zxj32 <zhaoxuj32@gmail.com>
2026-03-10 17:40:31 -0400

    test_branch

--- test.py
+++ test.py
@@ -5,3 +5,15 @@ import torch
 print(np.array([1, 2, 3, 4, 5]))
 
 print(torch.tensor([1, 2, 3, 4, 5]))
+
+print(torch.tensor([1, 2, 3, 4, 5]).numpy())
+
+print(torch.tensor([1, 2, 3, 4, 5]).tolist())
+
+print(torch.tensor([1, 2, 3, 4, 5]).to(torch.float32))
+
+print(torch.tensor([1, 2, 3, 4, 5]).to(torch.float64))
+
+print(torch.tensor([1, 2, 3, 4, 5]).to(torch.int32))
+
+print(torch.tensor([1, 2, 3, 4, 5]).to(torch.int64))
